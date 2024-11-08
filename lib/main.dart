f743dcd2783787e7d517309d5cca0d417e909766
faima-bittar <fatimabittar25@gmail.com>
2024-11-09 00:40:07 +0300

    adding column and nesting text widgets inside children

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,63 @@ void main() {
   runApp(MaterialApp(
     home: BasicProject(),
   ));
-} //material app
+}
 
-class BasicProject extends StatelessWidget{
-      @override
-  Widget build(BuildContext context){
-        return Scaffold(
-          backgroundColor: Colors.grey [850],
-          appBar: AppBar(
-            title: Text('Basic project card'),
-            centerTitle: true,
-            backgroundColor:  Colors.grey,
-            elevation: 0.0,
-          ), //AppBar
-        );//Scaffolf that allows us to make a layout like appbar and body ...
+class BasicProject extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      appBar: AppBar(
+        title: Text('Basic project card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ), //TextStyle
+            ), //Text
+            SizedBox(height: 10.0,),//adding space between two widgets
+            Text(
+              'Fatima',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ), //TextStyle
+            ), //Text
+            SizedBox(height: 30.0,),
+            Text(
+              'Your Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ), //TextStyle
+            ), //Text
+            SizedBox(height: 10.0,),//adding space between two widgets
+            Text(
+              '7',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ), //TextStyle
+            ), //Text
+            SizedBox(height: 30.0,),
+          ], //Widget[]
+        ),//Column
+      ),//Padding
+    ); //Scaffold
   }
 }
